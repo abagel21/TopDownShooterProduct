0021f5432068ab8cfc7f0a4cebb1fea4ce43f407
sassifr <alladai880@gmail.com>
2020-03-01 08:30:02 -0600

    added spacer to bottom of page, and blank about page

--- client/index.js
+++ client/index.js
@@ -2,7 +2,7 @@ function signin1(){
 	document.getElementById("up").style.display = "none";
 
 
-	document.getElementById("signwrap").style.height = "800px";
+	document.getElementById("spacer").style.height = "300px";
 
 	document.getElementById("password").classList.add("animation");
 	document.getElementById("username").classList.add("animation");
@@ -18,7 +18,8 @@ function signin1(){
 function signin2(){
 	document.getElementById("in").style.display = "none";
 
-	document.getElementById("signwrap").style.height = "800px";
+	document.getElementById("spacer").style.height = "300px";
+
 	//document.getElementById("password").style.color = "red";
 	//document.getElementById("password").style.background = "white";
 	//document.getElementById("password").style.display = "block";
@@ -37,6 +38,7 @@ function signin2(){
 function hideimg(){
 	document.getElementById("signup").style.height = "0px";
 	document.getElementById("signup").style.display = "none";
+
 }
 
 let form = document.getElementById('formwrap');
