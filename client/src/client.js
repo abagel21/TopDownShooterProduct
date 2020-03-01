c01d23f63fc95aa1ad6a7c8b5a295e0a42f445c8
nads <nads@gmail.com>
2020-03-01 01:58:45 -0600

    teams

--- client/src/client.js
+++ client/src/client.js
@@ -75,6 +75,7 @@ sock.on('usernameData', function(data){
     console.log(data.id);
     username = data.id;
 });
+
 let image = document.createElement('img');
 image.src = "img/cubeSpritesheetgreen.jpg";
 
@@ -90,6 +91,13 @@ sock.on('newPositions', (data) => {
             console.log('flip right');
             //image.classList.remove("img-hor");
         }
+        
+        if (data.player[i].team === "green"){
+            image.src = "img/cubeSpritesheetgreen.jpg";
+        } else {
+            image.src = "img/cubeSpritesheetred.jpg";
+        }
+        
         ctx.drawImage(image, data.player[i].imgX, data.player[i].imgY, 24, 24, data.player[i].x-25, data.player[i].y-25, 50, 50);
         ctx.font = '15px Arial';
         ctx.fillText(data.player[i].hp, data.player[i].x -10, data.player[i].y+40);
